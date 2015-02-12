ad3eb2393e60908ddef1a3046ff79f90edc0ec09
Andy Fleming <andy@andyfleming.com>
2015-02-12 12:40:13 -0800

    Starting basic version for testing

--- src/AggregateRelationshipsTrait.php
+++ src/AggregateRelationshipsTrait.php
@@ -2,6 +2,57 @@
 
 trait AggregateRelationshipsTrait {
 
+    public function generateAggregateAlias($className, $aggregateType)
+    {
+        if (strpos($className,'\\') !== false) {
+            $className = end(explode('\\',$className));
+        }
+
+        return snake_case($className).'_'.$aggregateType;
+    }
+
+    private function aggregateHasMany($aggregateType, $className, $foreignKey=null, $resultAlias=null, $aggregateTargetColumn)
+    {
+        $resultAlias = ($resultAlias)?: $this->generateAggregateAlias($className, $aggregateType);
+
+        if (!$foreignKey) {
+            $foreignKey = snake_case(get_called_class());
+        }
+
+        return $this->hasOne($className)
+            ->selectRaw(':foreign_key, count('.$aggregateTargetColumn.') as :aggregate_result_column_name',
+                [
+                    'foreign_key' => $foreignKey,
+                    'aggregate_result_column_name' => $resultAlias
+                ]
+            )
+            ->groupBy($foreignKey);
+    }
+
+    /**
+     * @param      $className - example - Comment
+     * @param null $foreignKey - example "article_id"
+     * @param null $countFieldName - example "comments_count"
+     *
+     * @return mixed
+     */
+    protected function countHasMany($className, $foreignKey=null, $countFieldName=null)
+    {
+
+        return $this->aggregateHasMany('count', $className, $foreignKey, $countFieldName, '*');
+
+    }
+
+    protected function averageHasMany()
+    {
+        // avg()
+    }
+
+    // min()
+
+    // max()
+
+    // sum()
 
 
 }
